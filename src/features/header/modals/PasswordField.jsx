8f3740aa84f26ffbaf822374dc3d7bcc6360d828
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-13 22:04:41 +0100

    include handle blur and useEffect to reset visibility

--- src/features/header/modals/PasswordField.jsx
+++ src/features/header/modals/PasswordField.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import styled from "styled-components";
+import { ModalContext } from "../../../app/providers/ModalProvider";
 
 function PasswordField({
   label,
@@ -8,30 +9,39 @@ function PasswordField({
   handlePassword,
   inputId,
   isInvalid,
+  handleBlur,
 }) {
+  const { modal } = useContext(ModalContext);
   const [visible, setVisible] = useState(false);
 
+  useEffect(() => {
+    setVisible(false);
+  }, [modal]);
+
   return (
-    <PasswordContainer>
-      <label htmlFor={inputId}>{label}</label>
-      <div className="input-container">
-        <input
-          className={isInvalid ? "invalid" : undefined}
-          type={visible ? "text" : "password"}
-          name={inputId}
-          id={inputId}
-          onChange={handlePassword}
-          value={passwordData}
-        />
-        <button
-          type="button"
-          onClick={() => setVisible((v) => !v)}
-          className="eye"
-        >
-          {visible ? <FaEyeSlash /> : <FaEye />}
-        </button>
-      </div>
-    </PasswordContainer>
+    <>
+      <PasswordContainer>
+        <label htmlFor={inputId}>{label}</label>
+        <div className="input-container">
+          <input
+            className={isInvalid ? "invalid" : undefined}
+            type={visible ? "text" : "password"}
+            name={inputId}
+            id={inputId}
+            onChange={handlePassword}
+            onBlur={handleBlur}
+            value={passwordData}
+          />
+          <button
+            type="button"
+            onClick={() => setVisible((v) => !v)}
+            className="eye"
+          >
+            {visible ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        </div>
+      </PasswordContainer>
+    </>
   );
 }
 
@@ -53,7 +63,7 @@ const PasswordContainer = styled.div`
     cursor: pointer;
     margin-top: auto;
     margin-bottom: auto;
-     top: 0;
+    top: 0;
     bottom: 0;
     right: 10px;
   }
