4c77b1e61369463e03392c59f26c69fd266e6b8f
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-13 21:52:35 +0100

    wrap router in auth provider

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import routesConfig from './router/routes.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routesConfig from "./router/routes.jsx";
+import { AuthProvider } from "./providers/AuthProvider.jsx";
 
 const routes = createBrowserRouter(routesConfig);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={routes}/>
+    <AuthProvider>
+      <RouterProvider router={routes} />
+    </AuthProvider>
   </StrictMode>,
-)
+);
