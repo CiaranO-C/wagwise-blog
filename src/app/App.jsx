8a8186c30885127758ab3363cba975633032641c
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-13 21:51:51 +0100

    write useEffect to load user in when token present

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,26 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../features/header/Header.jsx";
+import Footer from "../features/footer/Footer.jsx";
 import styled from "styled-components";
-import { ModalProvider } from "./provider";
+import { ModalProvider } from "./providers/ModalProvider.jsx";
+import { AuthContext } from "./providers/AuthProvider.jsx";
+import { useContext, useEffect, useState } from "react";
+import { userLoader } from "../api/user.js";
+import { getToken } from "../api/utils.js";
 
 function App() {
+  const { user, setUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function getUser() {
+      const user = await userLoader();
+      if (user) setUser(user.user);
+    }
+    if (!user && getToken()) {
+      getUser();
+    }
+  }, [user, setUser]);
+
   return (
     <ModalProvider>
       <Layout>
