70e3c3482693344c0aa0bc26969f016d1f4dc5bf
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-17 23:35:38 +0100

    write loader function for home page

--- src/app/router/loaders.js
+++ src/app/router/loaders.js
@@ -1,9 +1,28 @@
-import { mostRecent } from '../../api/article';
+import { getArticles, getRecentlyCommented } from "../../api/article";
+import { getTags } from "../../api/tags";
 
-async function homeLoader(){
-    const article = await mostRecent();
-   
-    return article;
+async function homeLoader() {
+  const articlesData = await getArticles();
+  const tagsData = await getTags();
+
+  if (articlesData.error || tagsData.error) {
+    const error = articlesData.error || tagsData.error;
+    throw new Response(null, {
+      status: error || 500,
+    });
+  }
+
+  const recentlyCommentedId = articlesData.articles.find(
+    (article) => article._count.comments > 0,
+  )?.id;
+
+  const { article } = await getRecentlyCommented(recentlyCommentedId);
+
+  return {
+    articles: articlesData.articles,
+    tags: tagsData.tags,
+    recentlyCommented: article,
+  };
 }
 
-export { homeLoader }
+export { homeLoader };
