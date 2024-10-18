2016d8a4b2625dbb14f640b2f3b9db95ada1fc26
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-18 15:47:45 +0100

    split article grid into own component

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,102 @@
+import { FaComment } from "react-icons/fa";
+import { IoMdHeart } from "react-icons/io";
+import styled from "styled-components";
+
+function ArticleCard({ article }) {
+  function stripTruncate(string) {
+    let stripped = string.split(/<\/?\w+[^>]*>/)[1];
+    if (stripped) {
+      const converted = convertHtmlEntities(stripped);
+      return converted;
+    }
+  }
+
+  function convertHtmlEntities(summary) {
+    if (typeof summary !== "string") {
+      console.error("Expected a string but got:", typeof summary);
+      return summary;
+    }
+    const htmlEntitiesMap = {
+      "&quot;": '"',
+      "&amp;": "&",
+      "&lt;": "<",
+      "&gt;": ">",
+      "&apos;": "'",
+    };
+
+    const converted = summary.replace(/&[a-zA-Z0-9#]+;/g, (match) => {
+      return htmlEntitiesMap[match] || match;
+    });
+    return converted;
+  }
+
+  return (
+    <Card>
+      <h3>{article.title}</h3>
+      <p className="article-body">{stripTruncate(article.body)}</p>
+      <div className="info">
+        <span>{new Date(article.created).toLocaleDateString()}</span>
+        <span>Written by: {article.author.username}</span>
+      </div>
+      <div className="icons">
+        <p>
+          {article._count.likes}
+          <IoMdHeart />
+        </p>
+        <p>
+          {article._count.comments}
+          <FaComment />
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 15px;
+  height: 100%;
+  box-shadow:
+    rgba(14, 30, 37, 0.12) 0px 2px 4px 0px,
+    rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+  background-color: cornsilk;
+  border-radius: 5px;
+
+  h3 {
+    min-height: 33px;
+    font-weight: 300;
+    border-bottom: 0.75px solid black;
+  }
+
+  .info {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .icons {
+    display: flex;
+    gap: 10px;
+
+    svg {
+      height: 17px;
+      width: 17px;
+    }
+
+    p {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+    }
+  }
+
+  .article-body {
+    padding-left: 5px;
+    border-left: 0.75px solid;
+    font-size: 1em;
+    margin-bottom: auto;
+  }
+`;
+
+export default ArticleCard;
--- src/components/ArticleGrid.jsx
+++ src/components/ArticleGrid.jsx
@@ -1,48 +1,15 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import ArticleCard from "./ArticleCard";
 
 function ArticleGrid({ children, articles }) {
-  function convertHtmlEntities(summary) {
-    if (typeof summary !== "string") {
-      console.error("Expected a string but got:", typeof summary);
-      return summary;
-    }
-    const htmlEntitiesMap = {
-      "&quot;": '"',
-      "&amp;": "&",
-      "&lt;": "<",
-      "&gt;": ">",
-      "&apos;": "'",
-    };
-
-    const converted = summary.replace(/&[a-zA-Z0-9#]+;/g, (match) => {
-      return htmlEntitiesMap[match] || match;
-    });
-    return converted;
-  }
-
-  function stripTruncate(string) {
-    let stripped = string.split(/<\/?\w+[^>]*>/)[1];
-    if (stripped) {
-      const converted = convertHtmlEntities(stripped);
-      return converted;
-    }
-  }
-
   return (
     <ArticleGridSection>
       {children}
       {articles.length ? (
         articles.map((article) => (
-          <Link key={article.id} to={`/admin/articles/${article.id}`}>
-            <ArticleCard>
-              <h3>{article.title}</h3>
-              <p className="article-body">{stripTruncate(article.body)}</p>
-              <div className="info">
-                <span>{new Date(article.created).toLocaleDateString()}</span>
-                <span>Written by: {article.author.username}</span>
-              </div>
-            </ArticleCard>
+          <Link key={article.id} to={`/article/${article.id}`}>
+            <ArticleCard article={article} />
           </Link>
         ))
       ) : (
@@ -57,6 +24,7 @@ const ArticleGridSection = styled.section`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: auto;
   gap: 15px;
+  max-width: 1200px;
 
   .empty-message {
     color: white;
@@ -67,31 +35,20 @@ const ArticleGridSection = styled.section`
     grid-column: 1 / -1;
   }
 
+  a > div {
+    transition: background-color 0.3s ease-out;
+  }
+
+  a:hover {
+    border-color: transparent;
+
+    & > div {
+      background-color: #fffef9;
+    }
+  }
+
   @media only screen and (max-width: 950px) {
     grid-template-columns: 1fr;
-  }
-`;
-
-const ArticleCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 15px;
-
-  h3 {
-    min-height: 60px;
-    font-weight: 300;
-    border-bottom: 0.75px solid black;
-  }
-
-  .info {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .article-body {
-    font-size: 1em;
-    flex: 1;
   }
 `;
 
