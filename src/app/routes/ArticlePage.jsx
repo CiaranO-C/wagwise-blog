1e135b5e90256294f4b2cba70e75d7af16a9e485
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-19 23:38:03 +0100

    scaffold ArticlePage component

--- src/app/routes/ArticlePage.jsx
+++ src/app/routes/ArticlePage.jsx
@@ -1,7 +1,27 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import ExpandableSearch from '../../components/ExpandableSearch';
+import { useRef } from 'react';
+import Footer from '../../features/footer/Footer';
+
 function ArticlePage() {
+    const openSearchRef = useRef(null);
+
   return (
-    <></>
-  )
+    <>
+    <ExpandableSearch intialPosition={false} buttonRef={openSearchRef}/>
+    <ArticleMain>
+      <Outlet />
+      <Footer />
+    </ArticleMain>
+    </>
+  );
 }
+
+const ArticleMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  grid-row: 2 / 4;
+`;
 
 export default ArticlePage;
