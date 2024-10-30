d731566b4400dc0e1dd2d5c3f5d5cd632d14f5cd
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-10-30 12:39:40 +0000

    remove old files, create utils for article feature

---
+++ src/features/article/utils.js
@@ -0,0 +1,24 @@
+function timeElapsed(date) {
+  const now = new Date();
+  const created = new Date(date);
+  const diffInSeconds = Math.floor((now - created) / 1000);
+
+  const seconds = diffInSeconds % 60;
+  const minutes = Math.floor(diffInSeconds / 60) % 60;
+  const hours = Math.floor(diffInSeconds / 3600) % 24;
+  const days = Math.floor(diffInSeconds / (3600 * 24));
+  const weeks = Math.floor(diffInSeconds / (3600 * 24 * 7));
+
+  if (weeks > 0) return `${weeks}w`;
+  if (days > 0) return `${days}d`;
+  if (hours > 0) return `${hours}h`;
+  if (minutes > 0) return `${minutes}m`;
+  return `${seconds}s`;
+}
+
+function sanitize(content) {
+    const sanitized = DOMPurify.sanitize(content);
+    return sanitized;
+  }
+
+export { timeElapsed, sanitize };
