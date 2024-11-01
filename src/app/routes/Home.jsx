636b74d8089aa4d4e2f430fc2ce05a413a15d688
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-01 18:05:01 +0000

    introduce useEffect in place of loader, sort data into props and useMemo

--- src/app/routes/Home.jsx
+++ src/app/routes/Home.jsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import ExpandableSearch from "../../components/ExpandableSearch";
-import { useMemo, useRef } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useMemo, useRef, useState } from "react";
 import Footer from "../../features/footer/Footer";
 import MostRecent from "../../features/home/MostRecent";
 import Divider from "../../features/home/divider/Divider";
 import MostPopular from "../../features/home/MostPopular";
 import Categories from "../../features/home/Categories";
+import { homeLoader } from "../router/loaders";
+import { MoonLoader } from "react-spinners";
+import Spinner from "../../components/Spinner";
 
 function Home() {
   const openSearchRef = useRef(null);
-  const { articles, tags, recentlyCommented } = useLoaderData();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const popularCount = 3;
 
@@ -20,25 +23,69 @@ function Home() {
     }
   }
 
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function getRouteData() {
+      const routeData = await homeLoader(controller.signal);
+      if (routeData) {
+        const { articles, tags } = routeData;
+        setData({ articles, tags });
+        setLoading(false);
+      }
+    }
+
+    if (!data && loading) {
+      getRouteData();
+    }
+
+    return () => {
+      controller.abort();
+    };
+  });
+
   const mostPopular = useMemo(() => {
-    const popular = articles
-      .toSorted((a, b) => b._count.likes - a._count.likes)
-      .slice(0, popularCount);
-    return popular;
-  }, [articles]);
+    if (data) {
+      const popular = data.articles
+        .toSorted((a, b) => b._count.likes - a._count.likes)
+        .slice(0, popularCount);
+      return popular;
+    }
+  }, [data]);
 
   const topTags = useMemo(() => {
-    const top = tags.toSorted((a, b) => b._count.articles - a._count.articles);
-    return top;
-  }, [tags]);
+    if (data) {
+      const top = data.tags.toSorted(
+        (a, b) => b._count.articles - a._count.articles,
+      );
+      return top;
+    }
+  }, [data]);
+
+  const recentlyCommentedId = useMemo(() => {
+    if (data) {
+      return data.articles.find((article) => article._count.comments > 0)?.id;
+    }
+  }, [data]);
+
+  if (loading)
+    return (
+      <Spinner
+        styles={{
+          gridRow: "2 / 3",
+          alignSelf: "center",
+          justifySelf: "center",
+        }}
+      />
+    );
 
   return (
     <>
       <ExpandableSearch intialPosition={false} buttonRef={openSearchRef} />
       <HomeMain>
         <Content>
-          <MostRecent article={articles[0]} />
-          <Divider openSearch={openSearch} article={recentlyCommented} />
+          <MostRecent article={data.articles[0]} />
+          <Divider openSearch={openSearch} mostRecent={data.articles[0].id} commented={recentlyCommentedId} />
           <Categories tags={topTags} />
           <MostPopular articles={mostPopular} />
         </Content>
