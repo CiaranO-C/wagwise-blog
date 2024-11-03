9dd02ccc1fd4c24ad8bde15112a41b5c1b433ca1
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-03 17:05:56 +0000

    update use effect to re-fetch articles when new category opened from same page

--- src/features/category/Category.jsx
+++ src/features/category/Category.jsx
@@ -1,32 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Spinner from "../../components/Spinner";
 import { categoryLoader } from "../../app/router/loaders";
 import { useParams } from "react-router-dom";
 import CategoryHeader from "./CategoryHeader";
 import RelatedCategories from "./RelatedCategories";
+import ArticlesSection from "./ArticlesSection";
 
 function Category() {
   const { name } = useParams();
   const [category, setCategory] = useState(null);
   const [loading, setLoading] = useState(true);
+  const previousCategory = useRef(null);
 
   useEffect(() => {
-    const controller = new AbortController();
+    if (previousCategory.current === name) return;
 
+    const controller = new AbortController();
     async function fetchCategory() {
       const categoryData = await categoryLoader(name, controller.signal);
 
       if (categoryData) {
+        previousCategory.current = name;
         setCategory(categoryData);
         setLoading(false);
       }
     }
-    if (!category && loading) fetchCategory();
+    setLoading(true);
+    fetchCategory();
 
     return () => {
       controller.abort();
     };
-  }, [category, loading, name]);
+  }, [name]);
 
   if (loading)
     return <Spinner styles={{ marginTop: "auto", placeSelf: "center" }} />;
@@ -39,6 +44,7 @@ function Category() {
         name={category.tagName}
         articleCount={category.articles.length}
       />
+      <ArticlesSection articles={category.articles} />
       <RelatedCategories
         current={category.tagName}
         articles={category.articles}
