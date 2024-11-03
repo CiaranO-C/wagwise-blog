4d2d634255c9e2e93dd854a11ea0520336f03120
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-03 17:20:19 +0000

    write error elements, improve handling of unexpected errors

--- src/app/routes/Error.jsx
+++ src/app/routes/Error.jsx
@@ -1,15 +1,44 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate, useRouteError } from "react-router-dom";
 import styled from "styled-components";
 import NotFound from "../../features/errors/404";
+import { useEffect } from "react";
+import Spinner from "../../components/Spinner";
+import Unauthorized from "../../features/errors/401";
+import ServerError from "../../features/errors/500";
 
 function Error() {
-  const { state: errorStatus } = useLocation();
-  NotFound;
+  const routeError = useRouteError();
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  useEffect(() => {
+    //if page renders as errorElement, navigate to styled error page within app
+    if (!state && routeError.status) {
+      return navigate("/error", { state: routeError.status });
+    }
+  });
+
+  const status = state || "unknown";
+
   const ErrorMap = {
     404: <NotFound />,
+    401: <Unauthorized />,
+    500: <ServerError />,
+    unknown: <ServerError />,
   };
 
-  return <Main>{ErrorMap[errorStatus]}</Main>;
+  if (!state && routeError.status)
+    return (
+      <Spinner
+        styles={{
+          gridRow: "2 / 3",
+          alignSelf: "center",
+          justifySelf: "center",
+        }}
+      />
+    );
+
+  return <Main>{ErrorMap[status]}</Main>;
 }
 
 const Main = styled.main`
