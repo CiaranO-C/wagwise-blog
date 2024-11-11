191786e8ea2e8c374d559cbb08b46a903c7099ac
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-11 00:33:08 +0000

    test about page renders ok

---
+++ test/about/About.test.jsx
@@ -0,0 +1,15 @@
+import { Route } from "react-router-dom";
+import About from "../../src/app/routes/About";
+import { render, screen } from "../CustomRender";
+import { vi } from 'vitest';
+
+beforeAll(() => {
+  window.scrollTo = vi.fn(); // Mock scrollTo
+});
+
+test("renders the About page", () => {
+  render(<Route path="about" element={<About />} />, {
+    initialEntries: ["/about"],
+  });
+  expect(screen.getByRole("heading", { name: "About" })).toBeInTheDocument();
+});
