65bb642ddaa3289c3b76b88d11dfd9463c6e90dd
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-12 17:41:19 +0000

    write tests for landing page

---
+++ test/App.test.jsx
@@ -0,0 +1,70 @@
+import { describe, expect, vi, beforeAll } from "vitest";
+import { render, screen } from "./CustomRender";
+import { Route } from "react-router-dom";
+import Landing from "../src/app/routes/Landing";
+import userEvent from "@testing-library/user-event";
+import Home from "../src/app/routes/Home";
+import { homeLoader } from "../src/app/router/loaders";
+import { mockArticles, mockTags } from './home/mock-data';
+
+beforeAll(() => {
+  window.scrollTo = vi.fn(); // Mock scrollTo
+});
+
+vi.mock("../src/app/router/loaders", () => ({
+  homeLoader: vi.fn(),
+}));
+
+describe("App Component Render Tests", () => {
+  test("Renders the Landing component as index route", () => {
+    render(<Route index element={<Landing />} />, {
+      initialEntries: ["/"],
+    });
+
+    expect(
+      screen.getByRole("heading", { name: "Empowering Owners & Dogs" }),
+    ).toBeInTheDocument();
+  });
+
+  test("Renders link to home page", () => {
+    render(<Route index element={<Landing />} />, {
+      initialEntries: ["/"],
+    });
+
+    expect(
+      screen.getByRole("link", { name: "Start Learning" }),
+    ).toBeInTheDocument();
+  });
+});
+
+describe("App component user event tests", () => {
+  const articles = mockArticles(20);
+  const tags = mockTags(8);
+
+  homeLoader.mockResolvedValue({
+    articles,
+    tags,
+  });
+  const user = userEvent.setup();
+
+  test("User can click link to home page", async () => {
+    render(
+      <>
+        <Route index element={<Landing />} />{" "}
+        <Route path="home" element={<Home />} />{" "}
+      </>,
+      {
+        initialEntries: ["/"],
+      },
+    );
+
+    const link = screen.getByRole("link", { name: "Start Learning" });
+
+    await user.click(link);
+
+    //home route should display most recent article heading
+    expect(
+      await screen.findByRole("heading", { name: "Most recent article:" }),
+    );
+  });
+});
