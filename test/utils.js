287e49b2b7ad7fb8fcced624c8bea3478d7f6397
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-11-12 14:41:11 +0000

    change date function to days since

--- test/utils.js
+++ test/utils.js
@@ -6,4 +6,10 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export { getRandomSuffix, randomInt };
+function daysSinceToday(days) {
+  const date = new Date();
+  date.setDate(date.getDate() - days); // Subtract the specified number of days
+  return date.toISOString(); // Return the ISO string of the updated date
+}
+
+export { getRandomSuffix, randomInt, daysSinceToday };
